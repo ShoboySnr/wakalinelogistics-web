23cb598000c495d71948866dcae37b852d954358
Olaniyi Olamide <olaniyilamie@gmail.com>
2026-03-27 14:15:37 +0100

    admin subscription mail template

---
+++ resources/views/emails/admin_new_subscription.blade.php
@@ -0,0 +1,8 @@
+<!doctype html>
+<html>
+<body>
+    <p>Admin,</p>
+    <p>A new user subscribed to the newsletter: <strong>{{ $email }}</strong></p>
+    <p>Check the dashboard for more details.</p>
+</body>
+</html>
