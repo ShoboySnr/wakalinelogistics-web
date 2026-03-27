b74e592771f024a9956755dfb6aed0b8e22d4cb9
Olaniyi Olamide <olaniyilamie@gmail.com>
2026-03-27 14:08:40 +0100

    public api routes for newsletter and contact form

---
+++ app/Modules/PublicApi/Routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Modules\PublicApi\Controllers\NewsletterController;
+use App\Modules\PublicApi\Controllers\ContactFormController;
+
+Route::prefix('v1/public')->name('api.public.')->middleware('api.token')->group(function () {
+    Route::post('newsletter/subscribe', [NewsletterController::class, 'subscribe'])->name('newsletter.subscribe');
+    Route::post('contact/enquiry', [ContactFormController::class, 'send'])->name('contact.send');
+});
