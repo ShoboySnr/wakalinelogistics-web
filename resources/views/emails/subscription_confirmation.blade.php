e717a2d56f0d59584efec27e43b94d51485a605d
Olaniyi Olamide <olaniyilamie@gmail.com>
2026-03-27 14:16:26 +0100

    subscription confirmation mail template

---
+++ resources/views/emails/subscription_confirmation.blade.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html>
+<body>
+    <p>Hi,</p>
+    <p>Thank you for subscribing to Wakaline. We're excited to keep you updated.</p>
+    <p>If you didn't sign up, please ignore this email.</p>
+    <p>Best regards,<br/>Wakaline Team</p>
+</body>
+</html>
