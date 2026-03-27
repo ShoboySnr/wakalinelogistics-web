f5535ba0847d1428e72f0ccc16551a323384fe3c
Olaniyi Olamide <olaniyilamie@gmail.com>
2026-03-27 14:14:50 +0100

    user subscription confirmation

---
+++ app/Mail/SubscriptionConfirmation.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use App\Models\Subscription;
+
+class SubscriptionConfirmation extends Mailable implements ShouldQueue
+{
+    use Queueable, SerializesModels;
+
+    public Subscription $subscription;
+
+    public function __construct(Subscription $subscription)
+    {
+        $this->subscription = $subscription;
+    }
+
+    public function build()
+    {
+        return $this->subject('Welcome to Wakaline')->view('emails.subscription_confirmation')
+            ->with(['email' => $this->subscription->email]);
+    }
+}
